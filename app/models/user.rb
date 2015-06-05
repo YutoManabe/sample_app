477fba02e290634cb763222a098bd5c6e1427dcc
Yuto Manabe <jddbyby@gmail.com>
2015-06-06 03:06:37 +0900

    Add email test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
 
 	# メールアドレスのフォーマット
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
 	# DB保存前に小文字へ変換する
-	before_save { self.email = email.downcase }
+	before_save { self.email = email.downcase! }
 
 	validates :name, presence: true, length: { maximum: 50 }
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
